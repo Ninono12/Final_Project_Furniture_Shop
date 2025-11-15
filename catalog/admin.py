d64cb46993ce18a89acc2135f7e9bffba95e9475
User <ninono12@gmail.com>
2025-11-15 23:14:27 +0400

    update Catalog

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from .models import Category, Product, Cart, CartItem, Order, OrderItem
+from django.contrib.auth.models import User
+from .models import Category, Product, Cart, CartItem, Order, OrderItem, CustomUserProfile
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'is_active', 'created_at')
     search_fields = ('name',)
     list_filter = ('is_active',)
-    #readonly_fields = ('slug',)
-    exclude = ('slug','description', 'image')
+    exclude = ('slug',)
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -36,4 +36,37 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('order_number', 'user', 'status', 'total_amount', 'created_at')
     search_fields = ('order_number', 'user__username')
     list_filter = ('status', 'created_at')
-    #inlines = [OrderItemInline]
+    inlines = [OrderItemInline]
+
+class CustomUserProfileInline(admin.StackedInline):
+    model = CustomUserProfile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+class CityFilter(admin.SimpleListFilter):
+    title = 'city'
+    parameter_name = 'city'
+
+    def lookups(self, request, model_admin):
+        cities = set(CustomUserProfile.objects.values_list('city', flat=True))
+        return [(c, c) for c in cities if c]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(custom_profile__city=self.value())
+        return queryset
+
+class CustomUserAdmin(admin.ModelAdmin):
+    inlines = (CustomUserProfileInline,)
+    list_display = ('username', 'email', 'get_full_name', 'is_staff')
+    search_fields = ('username', 'email', 'custom_profile__first_name', 'custom_profile__last_name', 'custom_profile__phone')
+    list_filter = (CityFilter,)
+
+    def get_full_name(self, obj):
+        if hasattr(obj, 'custom_profile'):
+            return obj.custom_profile.get_full_name()
+        return ''
+    get_full_name.short_description = 'Full Name'
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- catalog/models.py
+++ catalog/models.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.utils.text import slugify
 from django.db import models
-from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -137,6 +136,7 @@ class CustomUserProfile(models.Model):
     last_name = models.CharField(max_length=30)
     phone = models.CharField(max_length=20, blank=True)
     address = models.TextField(blank=True)
+    city = models.CharField(max_length=50, blank=True)
     birth_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
