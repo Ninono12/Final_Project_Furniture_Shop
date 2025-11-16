2b9e7f77a320dc46c7cf9fb6bb70cb3c09ea87f2
User <ninono12@gmail.com>
2025-11-17 01:26:39 +0400

    add files config/celery and catalog/tasks

---
+++ catalog/tasks.py
@@ -0,0 +1,26 @@
+from celery import shared_task
+from django.core.mail import send_mail
+from .models import Order
+from django.utils import timezone
+from datetime import timedelta
+
+@shared_task
+def send_order_confirmation_email(order_id):
+    try:
+        order = Order.objects.get(id=order_id)
+        send_mail(
+            subject=f"Order Confirmation #{order.id}",
+            message=f"Dear {order.user.username}, your order has been received!",
+            from_email='no-reply@furnitureshop.com',
+            recipient_list=[order.user.email],
+        )
+    except Order.DoesNotExist:
+        pass
+
+@shared_task
+def update_order_status():
+
+    pending_orders = Order.objects.filter(status='Pending', created_at__lte=timezone.now() - timedelta(minutes=5))
+    for order in pending_orders:
+        order.status = 'Processing'
+        order.save()
